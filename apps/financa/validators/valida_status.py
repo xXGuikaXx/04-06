a74540a8c1ceeae4492df21fff77b6cc82184824
victorsoaressn <kawangui2007@gmail.com>
2025-06-06 10:50:06 -0300

    Aula 06/06 uptate

--- apps/financa/validators/valida_status.py
+++ apps/financa/validators/valida_status.py
@@ -2,5 +2,5 @@ from django.core.validators import ValidationError
 
 def valida_status(status):
 
-    if status == 'E' or status == 'P':
-        raise ValidationError('Este valor deve ser do tipo de transacao.')
+    if status != 'E' or status != 'P':
+        raise ValidationError('Status invalido.')
